4f3be0c07dfc9e3ddc465de9cf64cf3f541058c1
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-14 23:00:20 -0300

    Componente Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { Button } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 
 export default function App(){
   return (
-    <>
+    <Container sx={{ border:4 }}>
       <h1>App</h1>
+      {/** Componente Typography:
+       * Le podemos pasar variantes con tamaños como h1, h2, h3 ...
+       * Tambien le podemos pasar un estilo x pero hacer que se
+       * renderice como otro estilo
+       * Podemos agregar propiedades como el textAling
+       */}
+      <Typography 
+      variant="h3"
+      Container="h4"
+      textAlign= "center"
+      > Texto desde el componente Tipography</Typography>
+      <Typography variant="h4"> Texto desde el componente Tipography</Typography>
+      <Typography variant="h5"> Texto desde el componente Tipography</Typography>
       <Button variant = "contained">Material UI</Button>
-    </>
+    </Container>
   )
 }
