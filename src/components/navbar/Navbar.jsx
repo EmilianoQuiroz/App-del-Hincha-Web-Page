8c4f5bd095145a097049b321da6b1d2836cdd542
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-29 22:57:28 -0300

    Drawer Component Material UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavListDrawer from './NavListDrawer'
+import { Button, Drawer } from '@mui/material'
 
 export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+
   return (
     <>
-        <NavListDrawer />
+        <Button variant="contained" onClick={() => setOpen(true)}>OpenDrawe</Button>
+        
+        <Drawer
+        open={open}
+        anchor="left"
+        onClose={() => setOpen(false)}
+        >
+            <NavListDrawer />
+        </Drawer>
     </>
   )
 }
