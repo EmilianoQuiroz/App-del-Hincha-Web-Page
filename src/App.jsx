1e03c32d80cfb7da4aeb05839673a159abc6af24
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-29 22:25:53 -0300

    Segunda practica con Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { Container } from '@mui/material'
+import { Container} from '@mui/material'
 // Practica de Botones en material UI
-import Botones from './test/botones'
+// import Botones from './test/botones'
 // Practica de como hacer una card en Material UI
-import Card1 from './test/Card1'
+// import Card1 from './test/Card1'
 // Segunda practica de como hacer una card en Material UI
-import Card2 from './test/Card2'
+// import Card2 from './test/Card2'
+
+
+
 
 const App = () => {
   return (
     <Container>
       {/**<Botones />*/}
       {/**<Card1 />*/}
-      <Card2 />
+      {/**<Card2 />*/}
     </Container>
   )
 }
